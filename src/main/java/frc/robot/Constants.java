28b979d0ccce84689f964de499ce04049c72ff68
Raallen314 <149740695+Raallen314@users.noreply.github.com>
2025-01-02 20:55:07 -0600

    errors fixed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,9 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import edu.wpi.first.math.util.Units;
 
 import com.revrobotics.CANSparkBase.IdleMode;
-import edu.wpi.first.math.util.Units;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
@@ -22,117 +23,105 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
   }
 
-  public static final class DriveConstants {
-    // Driving Parameters - Note that these are not the maximum capable speeds of
-    // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 4.8;//m/s
-    public static final double kMaxAngularSpeed = 3 * Math.PI; // radians per second
+    public static final class DriveConstants {
+        // Driving Parameters - Note that these are not the maximum capable speeds of
+        // the robot, rather the allowed maximum speeds
+        public static final double kMaxSpeedMetersPerSecond = 4.8;//m/s
+        public static final double kMaxAngularSpeed = 3 * Math.PI; // radians per second
 
-    public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+        public static final double kDirectionSlewRate = 1.2; // radians per second
+        public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
+        public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
 
-    // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(24.5);
-    // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(22.5);
-    // Distance between front and back wheels on robot
-    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+        // Chassis configuration
+        public static final double kTrackWidth = Units.inchesToMeters(24.5);
+        // Distance between centers of right and left wheels on robot
+        public static final double kWheelBase = Units.inchesToMeters(22.5);
+        // Distance between front and back wheels on robot
+        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-    // Angular offsets of the modules relative to the chassis in radians
-    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
-    public static final double kFrontRightChassisAngularOffset = 0;
-    public static final double kRearLeftChassisAngularOffset = Math.PI;
-    public static final double kRearRightChassisAngularOffset = Math.PI / 2;
+        // Angular offsets of the modules relative to the chassis in radians
+        public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+        public static final double kFrontRightChassisAngularOffset = 0;
+        public static final double kRearLeftChassisAngularOffset = Math.PI;
+        public static final double kRearRightChassisAngularOffset = Math.PI / 2;
 
-    // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 5;
-    public static final int kRearLeftDrivingCanId = 1;
-    public static final int kFrontRightDrivingCanId = 7;
-    public static final int kRearRightDrivingCanId = 3;
+        // SPARK MAX CAN IDs
+        public static final int kFrontLeftDrivingCanId = 5;
+        public static final int kRearLeftDrivingCanId = 1;
+        public static final int kFrontRightDrivingCanId = 7;
+        public static final int kRearRightDrivingCanId = 3;
 
-    public static final int kFrontLeftTurningCanId = 6;
-    public static final int kRearLeftTurningCanId = 2;
-    public static final int kFrontRightTurningCanId = 8;
-    public static final int kRearRightTurningCanId = 4;
+        public static final int kFrontLeftTurningCanId = 6;
+        public static final int kRearLeftTurningCanId = 2;
+        public static final int kFrontRightTurningCanId = 8;
+        public static final int kRearRightTurningCanId = 4;
 
-    public static final boolean kGyroReversed = true;
+        public static final boolean kGyroReversed = true;
 
-    public static final double kTurnControllerTolerance = 3;//degrees
-    public static final double kTurnControllerToleranceAcc = .5; //degrees/s
-    public static final double kSlowModeMultiplier = 0.25;
-    public static final double kFastModeMultiplier = 1;
-    public static final double kNormalModeMultiplier = 0.75; //0.25 is the slow mode
+        public static final double kTurnControllerTolerance = 3;//degrees
+        public static final double kTurnControllerToleranceAcc = .5; //degrees/s
+        public static final double kSlowModeMultiplier = 0.25;
+        public static final double kFastModeMultiplier = 1;
+        public static final double kNormalModeMultiplier = 0.75; //0.25 is the slow mode
+    }
 
     public static final class ModuleConstants {
-      // The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
-      // This changes the drive speed of the module (a pinion gear with more teeth will result in a
-      // robot that drives faster).
-      public static final int kDrivingMotorPinionTeeth = 14;
+        // The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
+        // This changes the drive speed of the module (a pinion gear with more teeth will result in a
+        // robot that drives faster).
+        public static final int kDrivingMotorPinionTeeth = 14;
 
-      // Invert the turning encoder, since the output shaft rotates in the opposite direction of
-      // the steering motor in the MAXSwerve Module.
-      public static final boolean kTurningEncoderInverted = true;
+        // Invert the turning encoder, since the output shaft rotates in the opposite direction of
+        // the steering motor in the MAXSwerve Module.
+        public static final boolean kTurningEncoderInverted = true;
 
-      // Calculations required for driving motor conversion factors and feed forward
-      public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
-      public static final double kWheelDiameterMeters = 0.0762; //meters
-      public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
-      // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15 teeth on the bevel pinion
-      public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
-      public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
-              / kDrivingMotorReduction;
+        // Calculations required for driving motor conversion factors and feed forward
+        public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
+        public static final double kWheelDiameterMeters = 0.0762; //meters
+        public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+        // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15 teeth on the bevel pinion
+        public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+        public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+                / kDrivingMotorReduction;
 
-      public static final double kDrivingEncoderPositionFactor = (kWheelDiameterMeters * Math.PI)
-              / kDrivingMotorReduction; // meters
-      public static final double kDrivingEncoderVelocityFactor = ((kWheelDiameterMeters * Math.PI)
-              / kDrivingMotorReduction) / 60.0; // meters per second
+        public static final double kDrivingEncoderPositionFactor = (kWheelDiameterMeters * Math.PI)
+                / kDrivingMotorReduction; // meters
+        public static final double kDrivingEncoderVelocityFactor = ((kWheelDiameterMeters * Math.PI)
+                / kDrivingMotorReduction) / 60.0; // meters per second
 
-      public static final double kTurningEncoderPositionFactor = (2 * Math.PI); // radians
-      public static final double kTurningEncoderVelocityFactor = (2 * Math.PI) / 60.0; // radians per second
+        public static final double kTurningEncoderPositionFactor = (2 * Math.PI); // radians
+        public static final double kTurningEncoderVelocityFactor = (2 * Math.PI) / 60.0; // radians per second
 
-      public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
-      public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
+        public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
+        public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
-      public static final double kDrivingP = 0.04;
-      public static final double kDrivingI = 0;
-      public static final double kDrivingD = 0;
-      public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
-      public static final double kDrivingMinOutput = -1;
-      public static final double kDrivingMaxOutput = 1;
+        public static final double kDrivingP = 0.04;
+        public static final double kDrivingI = 0;
+        public static final double kDrivingD = 0;
+        public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
+        public static final double kDrivingMinOutput = -1;
+        public static final double kDrivingMaxOutput = 1;
 
-      public static final double kTurningP = 1;
-      public static final double kTurningI = 0;
-      public static final double kTurningD = 0;
-      public static final double kTurningFF = 0;
-      public static final double kTurningMinOutput = -1;
-      public static final double kTurningMaxOutput = 1;
+        public static final double kTurningP = 1;
+        public static final double kTurningI = 0;
+        public static final double kTurningD = 0;
+        public static final double kTurningFF = 0;
+        public static final double kTurningMinOutput = -1;
+        public static final double kTurningMaxOutput = 1;
 
-      public static final IdleMode kDrivingMotorIdleMode = IdleMode.kBrake;
-      public static final IdleMode kTurningMotorIdleMode = IdleMode.kBrake;
+        public static final IdleMode kDrivingMotorIdleMode = IdleMode.kBrake;
+        public static final IdleMode kTurningMotorIdleMode = IdleMode.kBrake;
 
-      public static final int kDrivingMotorCurrentLimit = 50; // amps
-      public static final int kTurningMotorCurrentLimit = 20; // amps
-  }
-
-  public static final class NeoMotorConstants {
-        public static final double kFreeSpeedRpm = 5676; //RPM
+        public static final int kDrivingMotorCurrentLimit = 50; // amps
+        public static final int kTurningMotorCurrentLimit = 20; // amps
     }
 
-    public static final class ClimberConstants {
-        public static final int kMotorRightCanId = 17;
-        public static final int kMotorLeftCanId = 16;
-        public static final boolean kMotorInverted = false;
-        public static final double kPower = 0.5;
-        public static final double kRetractPower = -0.8;
-        public static final double kExtendPower = 0.8;
-        public static final float kMaxHeight = 190; // maximum height of climber arms in inches
-        public static final double kEncoderDistancePerRevolution = 1; // moteder ravlosen
-    }
-
+    public static final class NeoMotorConstants {
+      public static final double kFreeSpeedRpm = 5676; //RPM
   }
-}  
+}
