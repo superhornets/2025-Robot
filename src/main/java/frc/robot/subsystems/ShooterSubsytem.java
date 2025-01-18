38f053b3e6a0f0e5bda6e27df9fd44c00e62edaf
TheLettuc3 <123341702+TheLettuc3@users.noreply.github.com>
2025-01-18 10:30:29 -0600

    Commented out Shooter subsystem.

--- src/main/java/frc/robot/subsystems/ShooterSubsytem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsytem.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
@@ -15,15 +20,21 @@ import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterSubsytem {
-    private final SparkMax m_leftMotor = new SparkMax(ShooterConstants.kMotorLeftCanId, MotorType.kBrushless);
-    private final SparkMax m_rightMotor = new SparkMax(ShooterConstants.kMotorRightCanId, MotorType.kBrushless);
-    private final SparkClosedLoopController m_leftPIDController = m_leftMotor.getClosedLoopController();
-    private final SparkClosedLoopController m_rightPIDController = m_rightMotor.getClosedLoopController();
-    private final RelativeEncoder m_leftEncoder = m_leftMotor.getEncoder();
-    private final RelativeEncoder m_rightEncoder = m_rightMotor.getEncoder();
+    SparkMax m_leftMotor = new SparkMax(ShooterConstants.kMotorLeftCanId, MotorType.kBrushless);
+    SparkMax m_rightMotor = new SparkMax(ShooterConstants.kMotorRightCanId, MotorType.kBrushless);
+    SparkMaxConfig config = new SparkMaxConfig();
+
     private double goal = Double.NaN;
 
     public ShooterSubsytem() {
+        config.inverted(true);
+        config.idleMode(IdleMode.kBrake);
+        config.encoder.positionConversionFactor(1000);
+        config.encoder.velocityConversionFactor(1000);
+        config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+        config.closedLoop.pid(1.0, 0.0, 0.0);
+        //m_leftMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        //m_rightMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         /*    m_leftMotor.setInverted(ShooterConstants.kIsLeftMotorInverted);
         m_rightMotor.setInverted(ShooterConstants.kIsRightMotorInverted);
         
@@ -40,54 +51,53 @@ public class ShooterSubsytem {
         
         */
  }
-    public void runShooterSubwoofer() {
-        m_leftPIDController.setReference(ShooterConstants.kShooterSpeedSubwoofer, ControlType.kVelocity);
-        m_rightPIDController.setReference(ShooterConstants.kShooterSpeedSubwoofer, ControlType.kVelocity);
-        goal = ShooterConstants.kShooterSpeedSubwoofer;
-    }
-
-    public void runShooterPodium() {
-        m_leftPIDController.setReference(ShooterConstants.kShooterSpeedPodium, ControlType.kVelocity);
-        m_rightPIDController.setReference(ShooterConstants.kShooterSpeedPodium, ControlType.kVelocity);
-        goal = ShooterConstants.kShooterSpeedPodium;
-    }
-
-    public void runShooterAmp() {
-        m_leftPIDController.setReference(ShooterConstants.kShooterSpeedAmp, ControlType.kVelocity);
-        m_rightPIDController.setReference(ShooterConstants.kShooterSpeedAmp, ControlType.kVelocity);
-        goal = ShooterConstants.kShooterSpeedAmp;
-    }
-
-    public void stopShooter() {
-        m_leftPIDController.setReference(0, ControlType.kVelocity);
-        m_rightPIDController.setReference(0, ControlType.kVelocity);
-        goal = Double.NaN;
-    }
-
-    public void runShooterToInput(double speed) {
-        m_leftPIDController.setReference(speed, ControlType.kVelocity);
-        m_rightPIDController.setReference(speed, ControlType.kVelocity);
-    }
-
-    public boolean isAtSpeed() {
-        double lowerBound = goal - 300;
-        //double upperBound = goal + 300;
-
-        return ((m_leftEncoder.getVelocity() > lowerBound) /*&& (m_leftEncoder.getVelocity() < upperBound)*/)
+ /*  public void runShooterSubwoofer() {
+     m_leftPIDController.setReference(ShooterConstants.kShooterSpeedSubwoofer, ControlType.kVelocity);
+     m_rightPIDController.setReference(ShooterConstants.kShooterSpeedSubwoofer, ControlType.kVelocity);
+     goal = ShooterConstants.kShooterSpeedSubwoofer;
+ }
+ 
+ public void runShooterPodium() {
+     m_leftPIDController.setReference(ShooterConstants.kShooterSpeedPodium, ControlType.kVelocity);
+     m_rightPIDController.setReference(ShooterConstants.kShooterSpeedPodium, ControlType.kVelocity);
+     goal = ShooterConstants.kShooterSpeedPodium;
+ }
+ 
+ public void runShooterAmp() {
+     m_leftPIDController.setReference(ShooterConstants.kShooterSpeedAmp, ControlType.kVelocity);
+     m_rightPIDController.setReference(ShooterConstants.kShooterSpeedAmp, ControlType.kVelocity);
+     goal = ShooterConstants.kShooterSpeedAmp;
+ }
+ 
+ public void stopShooter() {
+     m_leftPIDController.setReference(0, ControlType.kVelocity);
+     m_rightPIDController.setReference(0, ControlType.kVelocity);
+     goal = Double.NaN;
+ }
+ 
+ public void runShooterToInput(double speed) {
+     m_leftPIDController.setReference(speed, ControlType.kVelocity);
+     m_rightPIDController.setReference(speed, ControlType.kVelocity);
+ }
+ 
+ public boolean isAtSpeed() {
+     double lowerBound = goal - 300;
+     //double upperBound = goal + 300;
+ 
+     return ((m_leftEncoder.getVelocity() > lowerBound) /*&& (m_leftEncoder.getVelocity() < upperBound)*/
         /*&& ((m_rightEncoder.getVelocity() > lowerBound) && (m_rightEncoder.getVelocity() < upperBound))*/;
-    }
-    @Override
-    public void periodic() {
-        SmartDashboard.putNumber("velocity", m_leftEncoder.getVelocity());
-        SmartDashboard.putBoolean("is at speed", isAtSpeed());
-        SmartDashboard.putNumber("Shooter left Voltage", m_leftMotor.getAppliedOutput() * m_leftMotor.getBusVoltage());
-        SmartDashboard.putNumber("Shooter left output current", m_leftMotor.getOutputCurrent());
-        SmartDashboard.putNumber("Shooter right Voltage",
-                m_rightMotor.getAppliedOutput() * m_rightMotor.getBusVoltage());
-        SmartDashboard.putNumber("Shooter right output current", m_rightMotor.getOutputCurrent());
-
-    }
-
+        /*  @Override
+        public void periodic() {
+            SmartDashboard.putNumber("velocity", m_leftEncoder.getVelocity());
+            SmartDashboard.putBoolean("is at speed", isAtSpeed());
+            SmartDashboard.putNumber("Shooter left Voltage", m_leftMotor.getAppliedOutput() * m_leftMotor.getBusVoltage());
+            SmartDashboard.putNumber("Shooter left output current", m_leftMotor.getOutputCurrent());
+            SmartDashboard.putNumber("Shooter right Voltage",
+                    m_rightMotor.getAppliedOutput() * m_rightMotor.getBusVoltage());
+            SmartDashboard.putNumber("Shooter right output current", m_rightMotor.getOutputCurrent());
+        
+        }
+        */
 }
 
 
