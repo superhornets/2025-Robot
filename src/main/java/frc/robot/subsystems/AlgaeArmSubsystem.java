9f3eba8323171b5097c244846ddd9865a6496c9d
TheLettuc3 <123341702+TheLettuc3@users.noreply.github.com>
2025-01-23 20:49:53 -0600

    Algae arm subsystem base code imported

--- src/main/java/frc/robot/subsystems/AlgaeArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeArmSubsystem.java
@@ -7,10 +7,13 @@ import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.AlgaeConstants;
@@ -56,9 +59,74 @@ private final Mechanism2d m_mech2d = new Mechanism2d(50, 50);
         ResetMode.kResetSafeParameters,
         PersistMode.kPersistParameters);
 
+        @SuppressWarnings("unused")
+        private final MechanismLigament2d intakePivotSecondMechanism =
+            intakePivotMechanism.append(
+                new MechanismLigament2d(
+                    "Intake Pivot Second Bar",
+                    SimulationRobotConstants.kIntakeLongBarLength
+                        * SimulationRobotConstants.kPixelsPerMeter,
+                    Units.radiansToDegrees(SimulationRobotConstants.kIntakeBarAngleRads)));
 
+    SmartDashboard.putData("Algae Subsystem", m_mech2d);
+    armEncoder.setPosition(0);
 
-
-        armEncoder.setPosition(0);
-    }
+    armMotorSim = new SparkFlexSim(armMotor, armMotorModel);
 }
+
+/** Zero the arm encoder when the user button is pressed on the roboRIO */
+  private void zeroOnUserButton() {
+    if (!wasReset && RobotController.getUserButton()) {
+      // Zero the encoder only when button switches from "unpressed" to "pressed" to prevent
+      // constant zeroing while pressed
+      wasReset = true;
+      armEncoder.setPosition(0);
+    } else if (!RobotController.getUserButton()) {
+      wasReset = false;
+    }
+  }
+
+  public Command stowCommand() {
+    return this.runOnce(
+        () -> {
+          stowWhenIdle = true;
+        });
+  }
+
+   @Override
+  public void periodic() {
+    zeroOnUserButton();
+
+    // Display subsystem values
+    SmartDashboard.putNumber("Algae/Arm/Position", armEncoder.getPosition());
+
+    // Update mechanism2d
+    intakePivotMechanism.setAngle(
+        Units.radiansToDegrees(SimulationRobotConstants.kIntakeMinAngleRads)
+            + Units.rotationsToDegrees(
+                armEncoder.getPosition() / SimulationRobotConstants.kIntakeReduction));
+  }
+
+  /** Get the current drawn by each simulation physics model */
+  public double getSimulationCurrentDraw() {
+    return m_intakeSim.getCurrentDrawAmps();
+  }
+
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+    m_intakeSim.setInput(armMotorSim.getAppliedOutput() * RobotController.getBatteryVoltage());
+
+    // Next, we update it. The standard loop time is 20ms.
+    m_intakeSim.update(0.020);
+
+    // Iterate the arm SPARK simulation
+    armMotorSim.iterate(
+        Units.radiansPerSecondToRotationsPerMinute(
+            m_intakeSim.getVelocityRadPerSec() * SimulationRobotConstants.kArmReduction),
+        RobotController.getBatteryVoltage(),
+        0.02);
+
+    // SimBattery is updated in Robot.java
+  }
+    }
+
