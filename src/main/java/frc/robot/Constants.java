8e0f404a091dd74c2dfadff4711db9107dcf2a0c
TheLettuc3 <123341702+TheLettuc3@users.noreply.github.com>
2025-01-23 20:36:23 -0600

    For Sophia

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -117,4 +117,31 @@ public final class Constants {
       public static final double kHold = 0.25;
     }
   }
+  public static final class SimulationRobotConstants {
+    public static final double kPixelsPerMeter = 20;
+
+    public static final double kElevatorGearing = 25; // 25:1
+    public static final double kCarriageMass =
+        4.3 + 3.15 + 0.151; // Kg, arm + elevator stage + chain
+    public static final double kElevatorDrumRadius = 0.0328 / 2.0; // m
+    public static final double kMinElevatorHeightMeters = 0.922; // m
+    public static final double kMaxElevatorHeightMeters = 1.62; // m
+
+    public static final double kArmReduction = 60; // 60:1
+    public static final double kArmLength = 0.433; // m
+    public static final double kArmMass = 4.3; // Kg
+    public static final double kMinAngleRads =
+        Units.degreesToRadians(-50.1); // -50.1 deg from horiz
+    public static final double kMaxAngleRads =
+        Units.degreesToRadians(40.9 + 180); // 40.9 deg from horiz
+
+    public static final double kIntakeReduction = 135; // 135:1
+    public static final double kIntakeLength = 0.4032262; // m
+    public static final double kIntakeMass = 5.8738; // Kg
+    public static final double kIntakeMinAngleRads = Units.degreesToRadians(80);
+    public static final double kIntakeMaxAngleRads = Units.degreesToRadians(180);
+    public static final double kIntakeShortBarLength = 0.1524;
+    public static final double kIntakeLongBarLength = 0.3048;
+    public static final double kIntakeBarAngleRads = Units.degreesToRadians(-60);
+  }
 }
--- src/main/java/frc/robot/subsystems/AlgaeArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeArmSubsystem.java
@@ -1,7 +1,64 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.Constants.AlgaeConstants;
+import frc.robot.Constants.SimulationRobotConstants;
 public class AlgaeArmSubsystem extends SubsystemBase {
     
+    private SparkFlex armMotor = new SparkFlex(AlgaeConstants.kPivotMotorCanId, MotorType.kBrushless);
+ private SparkClosedLoopController armController = armMotor.getClosedLoopController();
+  private RelativeEncoder armEncoder = armMotor.getEncoder();
+
+    private boolean stowWhenIdle = true;
+    private boolean wasReset = false;
+
+    private DCMotor armMotorModel = DCMotor.getNeoVortex(1);
+  private SparkFlexSim armMotorSim;
+  private final SingleJointedArmSim m_intakeSim =
+      new SingleJointedArmSim(
+          armMotorModel,
+          SimulationRobotConstants.kIntakeReduction,
+          SingleJointedArmSim.estimateMOI(
+              SimulationRobotConstants.kIntakeLength, SimulationRobotConstants.kIntakeMass),
+          SimulationRobotConstants.kIntakeLength,
+          SimulationRobotConstants.kIntakeMinAngleRads,
+          SimulationRobotConstants.kIntakeMaxAngleRads,
+          true,
+          SimulationRobotConstants.kIntakeMinAngleRads,
+          0.0,
+          0.0);
+
+private final Mechanism2d m_mech2d = new Mechanism2d(50, 50);
+  private final MechanismRoot2d m_mech2dRoot = m_mech2d.getRoot("Ball Intake Root", 28, 3);
+  private final MechanismLigament2d intakePivotMechanism =
+      m_mech2dRoot.append(
+          new MechanismLigament2d(
+              "Intake Pivot",
+              SimulationRobotConstants.kIntakeShortBarLength
+                  * SimulationRobotConstants.kPixelsPerMeter,
+              Units.radiansToDegrees(SimulationRobotConstants.kIntakeMinAngleRads)));
+
+    public class AlgaeSubsystem {
+    armMotor.configure(
+        Configs.AlgaeSubsystem.armConfig,
+        ResetMode.kResetSafeParameters,
+        PersistMode.kPersistParameters);
+
+
+
+
+        armEncoder.setPosition(0);
+    }
 }
