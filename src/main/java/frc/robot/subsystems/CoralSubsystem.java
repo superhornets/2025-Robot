269233b54e1405fe7ff7d105dd85796382484936
Sophia <173519141+Sophiaphillips414@users.noreply.github.com>
2025-02-01 11:11:21 -0600

    fixed errors

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs;
+import frc.robot.Constants.CoralConstants;
 
 public class CoralSubsystem extends SubsystemBase {
     private final SparkMax motor = new SparkMax(CoralConstants.kCanId, MotorType.kBrushless);
@@ -13,8 +18,7 @@ public class CoralSubsystem extends SubsystemBase {
     public CoralSubsystem() {
         SparkMaxConfig config = new SparkMaxConfig();
         config.inverted(true);
-        config.encoder.convertionFactor(20) ;
-        motor.configure(config, );
+        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
         this.setDefaultCommand(new RunCommand(() -> {
         }, this));
     }
@@ -26,6 +30,11 @@ public class CoralSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
     }
 
 }
