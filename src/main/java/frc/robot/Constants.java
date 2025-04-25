9e9de8d187aa48021d73e3f48513252b2dc5beee
Raallen314 <149740695+Raallen314@users.noreply.github.com>
2025-04-24 19:30:33 -0500

    changed to slow mode

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,8 +68,8 @@ public final class Constants {
         public static final boolean kGyroReversed = true;
 
         public static final double kSlowModeMultiplier = 0.25;
-        public static final double kFastModeMultiplier = 1;
-        public static final double kNormalModeMultiplier = 0.25; //0.25 is the slow mode
+        public static final double kFastModeMultiplier = 0.25; //1 is normal
+        public static final double kNormalModeMultiplier = 0.25; //0.75 is normal
 
         public static final double kDirectionSlewRate = 1.2; // radians per second
         public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
