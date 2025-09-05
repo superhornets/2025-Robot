8057e4314f8ae18c14b88dbb5adb4fbd46929694
Raallen314 <149740695+Raallen314@users.noreply.github.com>
2025-09-04 21:16:53 -0500

    changed to slow mode

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -66,8 +66,8 @@ public final class Constants {
         public static final double kTurnControllerTolerance = 3;//degrees
         public static final double kTurnControllerToleranceAcc = .5; //degrees/s
         public static final double kSlowModeMultiplier = 0.25;
-        public static final double kFastModeMultiplier = 1;
-        public static final double kNormalModeMultiplier = 0.75; //0.25 is the slow mode
+        public static final double kFastModeMultiplier = 0.25;
+        public static final double kNormalModeMultiplier = 0.25; //0.25 is the slow mode
     }
 
     public static final class ModuleConstants {
